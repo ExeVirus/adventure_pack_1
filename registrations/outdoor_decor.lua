d1f2ca3e012243ad53babcf0911c3f6d2f6b1d19
ExeVirus <ddsallad@gmail.com>
2021-01-01 23:05:30 -0500

    45%

--- registrations/outdoor_decor.lua
+++ registrations/outdoor_decor.lua
@@ -1,22 +1,126 @@
 node_definition ={
+	description =  "adventure_pack_1:berry_bush",
+	drawtype = "mesh",
+	mesh = "berry_bush.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"berry_bush.png"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:berry_bush","braizer.box",node_definition,true)
+
+node_definition ={
 	description =  "adventure_pack_1:ballista",
 	drawtype = "mesh",
-        mesh = "ballista.obj",
-        sunlight_propagates = true,
-        paramtype2 = "facedir",
-        tiles = {"ballista.png"},
-        groups = { oddly_breakable_by_hand=2 },
+	mesh = "ballista.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"ballista.png"},
+	groups = { oddly_breakable_by_hand=2 },
 }
 autobox.register_node("adventure_pack_1:ballista","ballista.box",node_definition,true)
 
--- bench
--- bleachers
--- bonfire
--- bonfire_2
--- braizer
--- broken_minecart
--- cannon
--- cart_empty
+node_definition ={
+	description =  "adventure_pack_1:bench",
+	drawtype = "mesh",
+	mesh = "bench.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"bench.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:bench","bench.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:bleachers",
+	drawtype = "mesh",
+	mesh = "bleachers.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"bleachers.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:bleachers","bleachers.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:bonfire",
+	drawtype = "mesh",
+	mesh = "bonfire.obj",
+	light_source = 12,
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"bonfire.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:bonfire","bonfire.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:bonfire_2",
+	drawtype = "mesh",
+	light_source = 12,
+	mesh = "bonfire_2.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"bonfire_2.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:bonfire_2","bonfire_2.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:braizer",
+	drawtype = "mesh",
+	light_source = 7,
+	mesh = "braizer.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"braizer.png"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:braizer","braizer.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:broken_minecart",
+	drawtype = "mesh",
+	mesh = "broken_minecart.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"broken_minecart.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:broken_minecart","broken_minecart.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:cannon",
+	drawtype = "mesh",
+	mesh = "cannon.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"cannon.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:cannon","cannon.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:cart_empty",
+	drawtype = "mesh",
+	mesh = "cart_empty.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"cart_empty.jpg"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:cart_empty","cart_empty.box",node_definition,true)
+
+node_definition ={
+	description =  "adventure_pack_1:cart_goods",
+	drawtype = "mesh",
+	mesh = "cart_goods.obj",
+	sunlight_propagates = true,
+	paramtype2 = "facedir",
+	tiles = {"cart_goods.png"},
+	groups = { oddly_breakable_by_hand=2 },
+}
+autobox.register_node("adventure_pack_1:cart_goods","cart_goods.box",node_definition,true)
 -- cart_goods
 -- city_gibbet
 -- city_gibbet_filled
